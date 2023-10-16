4cee650574f6684fe502e1fc6444cac9ee3f84a1
decadev Ikebobo <ikennaamadi17@gmail.com>
2023-10-16 08:29:34 -0700

    my first boolean task

---
+++ src/main/java/org/hotel/algorithm/TesPersonal1.java
@@ -0,0 +1,15 @@
+package org.hotel.algorithm;
+
+public class TesPersonal1 {
+    public static void main(String[] args) {
+        int  num = 1568684;
+        if  (num % 2==0) {
+            System.out.println("even num");
+        }
+        else {
+                System.out.println("odd num");
+            }
+        }
+
+    }
+
