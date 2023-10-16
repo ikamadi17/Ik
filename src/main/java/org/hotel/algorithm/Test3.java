d7c8ce8f90ab2de5869d32b75d210f0839dcf9e8
decadev Ikebobo <ikennaamadi17@gmail.com>
2023-10-16 08:28:02 -0700

    my first boolean task

---
+++ src/main/java/org/hotel/algorithm/Test3.java
@@ -0,0 +1,11 @@
+package org.hotel.algorithm;
+
+public class Test3 {
+    public static void main(String[] args) {
+    String[] arrayOfThings = {"HOUSE", "CAR", "MONEY", "JEWELRIES", "ACCESSORIES", "DOCUMENTS", "LAPTOPS"};
+
+    for (int i = 0; i < arrayOfThings.length; i++) {
+        System.out.println("Index: "+ 1 +" Element: "+ arrayOfThings[i]);
+    }
+    }
+}
