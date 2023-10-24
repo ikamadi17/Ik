1a46bb8e776108c3858a2779ad9e63c3a8c9e108
decadev Ikebobo <ikennaamadi17@gmail.com>
2023-10-23 17:00:06 -0700

    my first boolean task

---
+++ src/main/java/org/hotel/algorithm/ForLoopTest1.java
@@ -0,0 +1,7 @@
+package org.hotel.algorithm;
+
+public class LoopTest1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/hotel/algorithm/HomeTryOuts.java
@@ -0,0 +1,2 @@
+package org.hotel.algorithm;public class HomeTryOuts {
+}
